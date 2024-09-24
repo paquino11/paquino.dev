d8f9e0120a096455ea7e62ed67f3cbfce4edefe0
Pedro Aquino <pedro.aquino479@gmail.com>
2024-09-24 20:24:51 +0200

    update

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -9,7 +9,7 @@ export function Intro() {
                 <h1 className="text-4xl intro-card-welcome">sup guys</h1>
             </div>
             <div className="intro-card-body">
-                <p className="text-base body-md">i&apos;m a software engineer obsessed with building and learning new stuff (mostly backend and AI, you can notice by this portfolio frontend is not my best skill ;) long live aceternityUI and prompt engineering.</p>
+                <p className="text-base body-md">i&apos;m a software engineer obsessed with building and learning new stuff (mostly backend, frontend, AI and automation).</p>
                 <p className="text-base body-md">currently focused on building AI products with open-source frameworks LangGraph, crewAI, and LLamaIndex (yes, I&apos;m not stuck to only one :)), CI/CD pipelines, AI automation, and AI agents using paid LLMs, please sponsor me OpenAI and Anthropic, since I don&apos;t have enough computer power to run Llama 3.1 405B :(</p>
                 <p className="text-base body-md">i am always open to new work opportunities or consulting related to gen AI applications, full-stack dev or automation. To get in touch with me quickly, you can send me an email.</p>
                 <div className="flex flex-row gap-[4px]">
